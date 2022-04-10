649d8dd48cdcd44a467c568f65638455d01b6853
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-04-10 21:40:11 +0200

    194 show the entries in thr RV in the mainActivity done

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
 import com.example.happyplaces.models.HappyPlaceModel
@@ -25,6 +28,15 @@ class MainActivity : AppCompatActivity() {
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+
+        binding?.rvHappyPlacesList?.layoutManager = LinearLayoutManager(this)
+        binding?.rvHappyPlacesList?.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        binding?.rvHappyPlacesList?.adapter = placesAdapter
+    }
+
     private fun getHappyPlacesListFromLocalDB() {
 
         val dbHandler = DatabaseHandler(this)
@@ -32,11 +44,21 @@ class MainActivity : AppCompatActivity() {
         val getHappyPlacesList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if (getHappyPlacesList.size > 0) {
-            for (i in getHappyPlacesList){
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
-            }
-        }
 
+            ///// Prepare the Views
+            binding?.rvHappyPlacesList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+
+            ///// Call the recyclerView fun
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+
+
+        }else{
+            ///// Hide the Views
+            binding?.rvHappyPlacesList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
+
+        }
     }
+
 }
